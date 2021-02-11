02c79218111b853fcc53c5ce447cce72fe065c20
Felipe Martins <felipemjesus@outlook.com>
2021-02-10 22:10:07 -0300

    Testes automatizados manualmente

--- teste_avaliador.php
+++ teste_avaliador.php
@@ -7,6 +7,8 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Prepara dados para o teste
+// Arrange - Given
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -15,9 +17,19 @@ $joao = new Usuario('João');
 $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
+// Executo o código a ser testado
+// Act - When
 $leiloeiro = new Avaliador();
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor . PHP_EOL;
+// Verifico se a saída é a esperada
+// Assert - Then
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+    echo "TESTE OK" . PHP_EOL;
+} else {
+    echo "TESTE FALHOU" . PHP_EOL;
+}
